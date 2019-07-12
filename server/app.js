4b7f75a511db2572c9c96fc8926bd3017b1237a2
Santana <nsantana@estee.com>
2019-07-12 09:29:44 -0400

    testing stagin

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const process = require("process");
 //changes
 //working with other stuff
 // viewed at http://localhost:3000
-//testing production branch
+//testing stagint to production
 app.get("/api/:id", function(req, res) {
   res.sendFile(path.join(__dirname + "/manifest.json"));
 });
