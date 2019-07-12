847bd121b879762d265659f8dd731880aa81e2a6
Santana <nsantana@estee.com>
2019-07-12 09:46:40 -0400

    checking branch

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const https = require("https");
 const process = require("process");
 
 // viewed at http://localhost:3000
-//testing staging to production!
+//testing staging to production
 app.get("/api/:id", function(req, res) {
   res.sendFile(path.join(__dirname + "/manifest.json"));
 });
