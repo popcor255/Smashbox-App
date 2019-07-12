c78a157db53aef77e45caf8b5ba992010c48ba4e
Santana <nsantana@estee.com>
2019-07-12 10:10:10 -0400

    another test

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const https = require("https");
+const process = require("process");
+
+//changes
+//another one
+//another change
+//working with other stuff
+// viewed at http://localhost:3000
+//testing staging to productionww
+app.get("/api/:id", function(req, res) {
+  res.sendFile(path.join(__dirname + "/manifest.json"));
+});
+
+app.use(express.static(__dirname + "/build"));
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000");
+});
