e846992b8d985d1ff4ebc9fd7f794b9638266cba
Santana <nsantana@estee.com>
2019-08-07 01:02:29 -0400

    fix ssl path

--- build/app/app.js
+++ build/app/app.js
@@ -10,9 +10,9 @@ const request = require("request");
 const nodemailer = require("nodemailer");
 
 
-const cert = fs.readFileSync('./ssl/www_actuallythe_best.crt');
-const ca = fs.readFileSync('./ssl/www_actuallythe_best.ca-bundle');
-const key = fs.readFileSync('./ssl/example_com.key');
+const cert = fs.readFileSync('../ssl/www_actuallythe_best.crt');
+const ca = fs.readFileSync('../ssl/www_actuallythe_best.ca-bundle');
+const key = fs.readFileSync('../ssl/example_com.key');
 
 let httpsOptions = {
     cert: cert, // fs.readFileSync('./ssl/example.crt');
